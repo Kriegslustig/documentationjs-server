08b124ac51d93e519cb4f7b18b14f365934eb162
Luca Nils Schmid <me@lucaschmid.net>
2017-07-05 23:12:11 +0200

    feat: implement assets routes

--- index.js
+++ index.js
@@ -20,23 +20,35 @@ app.get('/:organization/:repository', (req, res) => {
   const { organization, repository } = req.params
   const url = gitUrl(organization, repository)
   const cached = cache.get(url) || {}
-  const respond = (html) => {
-    res.type('html').end(html)
+  const respond = (files) => {
+    const html = files[files.length - 1]
+    res.type('html').end(html.contents.toString())
   }
 
   if (cached[ref]) {
     respond(cached[ref])
   } else {
     generateDocumentation(url, ref)
-      .then((html) => {
-        cache.set(url, Object.assign({}, cached, { [ref]: html }))
-        respond(html)
+      .then((files) => {
+        cache.set(url, Object.assign({}, cached, { [ref]: files }))
+        respond(files)
       })
       .catch((err) => {
         console.error(err)
         res.status(500).end()
       })
   }
+})
+app.get('/:organization/:repository/*', (req, res) => {
+  const ref = req.query.ref || 'master'
+  const { organization, repository } = req.params
+  const url = gitUrl(organization, repository)
+  const cached = cache.get(url) || {}
+  const filepath = req.params[0]
+  if (!cached[ref]) return res.status(404).end()
+  const file = cached[ref].find((f) => f.relative === filepath)
+  if (!file) return res.status(404).end()
+  res.status(200).end(file.contents.toString())
 })
 
 app.listen(config.port)
